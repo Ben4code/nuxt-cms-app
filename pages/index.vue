fafb3e3a37786412df606aa7db56412e9ce48844
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2019-01-03 12:30:46 +0100

    Done with post listing page and sidenav toggle

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,61 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        testapp
-      </h1>
-      <h2 class="subtitle">
-        My slick Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news.</h1>
+    </section>
+    <section class="featured-posts">
+
+      <posts-preview id="1" title="Post Title 1" previewText="Preview Text 1" thumbnail="https://www.codingdojo.com/blog/wp-content/uploads/tech-literacy-Copy.gif"/>
+      
+      <posts-preview id="2" title="Post Title 2" previewText="Preview Text 2" thumbnail="https://pro2-bar-s3-cdn-cf.myportfolio.com/ec4657434c011e1a856a01752ef5f2f5/bf15faa73b4e4fa5c0bea7ae4afd56bb40a1d4cf06f2f73c349ebc640c3ceac9441990f6ec55da25_rw_1920.gif?h=29f18d9bcf1a2a651316b564f047596c"/>
+
+      <posts-preview id="3" title="Post Title 3" previewText="Preview Text 3" thumbnail="https://cdn.dribbble.com/users/214929/screenshots/4366947/dribbble-shot_6.gif"/>
+
+    </section>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import PostsPreview from '~/components/PostsPreview'
 
 export default {
   components: {
-    Logo
+    PostsPreview
   }
 }
 </script>
 
-<style>
+<style >
+    .intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: contain;
+  background-image: url('~assets/images/nuxt-fundamentals.png');
+  
 
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
 }
 </style>
