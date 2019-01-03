bc08cec73cdc7dee8d855849d0d3d0316e752189
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2019-01-03 09:43:21 +0100

    Initial app setup

---
+++ pages/posts/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="posts">
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.posts{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+</style>
